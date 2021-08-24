d28830b227bdf3fbe8108f5a213894047416e119
dusiko <zheka_yura@ukr.net>
2021-08-24 10:37:25 +0300

    ок

--- sudoku/js/script.js
+++ sudoku/js/script.js
@@ -1,8 +1,8 @@
 let infoItems = {
 	'board': document.getElementById('board'),
-	'itemsNumsArr' : [],
-	'moveCount' : 0,
-	
+	'itemsNumsArr': [],
+	'moveCount': 0,
+
 }
 let cords = {}
 
@@ -27,12 +27,12 @@ function createGameboard() {
 			item.classList.add('items')
 			item.setAttribute('data-y', i)
 			item.setAttribute('data-x', k)
-			if (count < 16){
-			do{
-			  infoItems.num = 1 + Math.floor(Math.random() * (15 + 1 - 1))
-			} while (infoItems.itemsNumsArr.includes(infoItems.num))
-			infoItems.itemsNumsArr.push(infoItems.num)
-		  item.innerText = infoItems.num
+			if (count < 16) {
+				do {
+					infoItems.num = 1 + Math.floor(Math.random() * (15 + 1 - 1))
+				} while (infoItems.itemsNumsArr.includes(infoItems.num))
+				infoItems.itemsNumsArr.push(infoItems.num)
+				item.innerText = infoItems.num
 			} else
 				item.classList.add('empty')
 			infoItems.board.appendChild(item)
@@ -51,73 +51,73 @@ function moveItem(event) {
 		cords.yTarget = +event.target.dataset.y;
 		cords.xEmpty = +cords.empty.dataset.x;
 		cords.yEmpty = +cords.empty.dataset.y;
-		
-		if (cords.xTarget === cords.xEmpty && cords.yTarget === cords.yEmpty - 1 && cords.yTarget > 0)
-		moveIf()
-			
-		if (cords.xTarget === cords.xEmpty - 1 && cords.yTarget === cords.yEmpty && cords.xTarget > 0)
-		moveIf()
-		
-		if (cords.xTarget === cords.xEmpty + 1 && cords.yTarget === cords.yEmpty && cords.xTarget < 5) 
-		moveIf()
 
-		if (cords.xTarget === cords.xEmpty && cords.yTarget === cords.yEmpty + 1 && cords.yTarget < 5) 
-		moveIf()
-		
+		if (cords.xTarget === cords.xEmpty && cords.yTarget === cords.yEmpty - 1 && cords.yTarget > 0)
+			moveIf()
+
+		if (cords.xTarget === cords.xEmpty - 1 && cords.yTarget === cords.yEmpty && cords.xTarget > 0)
+			moveIf()
+
+		if (cords.xTarget === cords.xEmpty + 1 && cords.yTarget === cords.yEmpty && cords.xTarget < 5)
+			moveIf()
+
+		if (cords.xTarget === cords.xEmpty && cords.yTarget === cords.yEmpty + 1 && cords.yTarget < 5)
+			moveIf()
+
 		check()
 	}
 }
 
-function moveIf(){
-			event.target.setAttribute('data-y', cords.yEmpty)
-			event.target.setAttribute('data-x', cords.xEmpty)
-			cords.empty.setAttribute('data-x', cords.xTarget)
-			cords.empty.setAttribute('data-y', cords.yTarget)
-			let itemNextSubl = event.target.nextSibling
-			if (itemNextSubl != null && !itemNextSubl.classList.contains('empty')) {
-				cords.empty.replaceWith(event.target)
-				infoItems.board.insertBefore(cords.empty, itemNextSubl)
-			} else if (itemNextSubl === null) {
-				itemNextSubl = event.target
-				cords.empty.replaceWith(event.target)
-				infoItems.board.appendChild(cords.empty)
-			} else if (itemNextSubl.classList.contains('empty')) {
-				itemNextSubl = event.target
-				cords.empty.replaceWith(event.target)
-				infoItems.board.insertBefore(cords.empty, itemNextSubl)
-			}
-			infoItems.moveCount++
-			document.querySelector('.stepsCount').innerText = infoItems.moveCount
-			if(infoItems.moveCount < 150)
-			document.querySelector('.stepsCount').style.color = 'green'
-			else if (infoItems.moveCount < 250 && infoItems.moveCount > 149)
-			  document.querySelector('.stepsCount').style.color = 'orange'
-			else
-			   document.querySelector('.stepsCount').style.color = 'red'
+function moveIf() {
+	event.target.setAttribute('data-y', cords.yEmpty)
+	event.target.setAttribute('data-x', cords.xEmpty)
+	cords.empty.setAttribute('data-x', cords.xTarget)
+	cords.empty.setAttribute('data-y', cords.yTarget)
+	let itemNextSubl = event.target.nextSibling
+	if (itemNextSubl != null && !itemNextSubl.classList.contains('empty')) {
+		cords.empty.replaceWith(event.target)
+		infoItems.board.insertBefore(cords.empty, itemNextSubl)
+	} else if (itemNextSubl === null) {
+		itemNextSubl = event.target
+		cords.empty.replaceWith(event.target)
+		infoItems.board.appendChild(cords.empty)
+	} else if (itemNextSubl.classList.contains('empty')) {
+		itemNextSubl = event.target
+		cords.empty.replaceWith(event.target)
+		infoItems.board.insertBefore(cords.empty, itemNextSubl)
+	}
+	infoItems.moveCount++
+	document.querySelector('.stepsCount').innerText = infoItems.moveCount
+	if (infoItems.moveCount < 150)
+		document.querySelector('.stepsCount').style.color = 'green'
+	else if (infoItems.moveCount < 250 && infoItems.moveCount > 149)
+		document.querySelector('.stepsCount').style.color = 'orange'
+	else
+		document.querySelector('.stepsCount').style.color = 'red'
 }
-function check(){
-  const items = document.querySelectorAll('.items')
-  infoItems.itemsNumsArr.length = 0
-  items.forEach((item) => {
-    infoItems.itemsNumsArr.push(item.innerText)
-  })
-// check
-  infoItems.check = 0
-  infoItems.itemsNumsArr.forEach((val,i,arr) => {
-    if(i+1 == val){
-    infoItems.check++
-    } else if (infoItems.check === 15)
-      win()
-      else if (infoItems.check === 13 && arr[15] == '' && arr[14] == 14 && arr[13] == 15)
-      win()
-      return
-  })
+function check() {
+	const items = document.querySelectorAll('.items')
+	infoItems.itemsNumsArr.length = 0
+	items.forEach((item) => {
+		infoItems.itemsNumsArr.push(item.innerText)
+	})
+	// check
+	infoItems.check = 0
+	infoItems.itemsNumsArr.forEach((val, i, arr) => {
+		if (i + 1 == val) {
+			infoItems.check++
+		} else if (infoItems.check === 15)
+			win()
+		else if (infoItems.check === 13 && arr[15] == '' && arr[14] == 14 && arr[13] == 15)
+			win()
+		return
+	})
 }
-function win(){
-  const div = document.createElement('DIV')
-  const gameDiv = document.getElementById('game')
-  let steps = document.querySelector('.stepsCount').innerText
-  div.classList.add('win')
-  div.innerHTML = 'You Win!!'+`<br>Your score: `+ steps
-  gameDiv.appendChild(div)
+function win() {
+	const div = document.createElement('DIV')
+	const gameDiv = document.getElementById('game')
+	let steps = document.querySelector('.stepsCount').innerText
+	div.classList.add('win')
+	div.innerHTML = 'You Win!!' + `<br>Your score: ` + steps
+	gameDiv.appendChild(div)
 }
