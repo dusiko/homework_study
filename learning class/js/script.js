74d82a7355089bfe35b8fae511408bc3555c09f0
Юрий Жека <75530764+dusiko@users.noreply.github.com>
2021-09-24 18:37:57 +0300

    Iii

--- learning class/js/script.js
+++ learning class/js/script.js
@@ -1,79 +1,118 @@
-class Game {
-	constructor(row, colm, mines, container) {
-		this.row = row
-		this.colm = colm
-		this.mines = mines
-		this.container = container
-		let tableArr = []
-		let minesArr = []
-		let mine
-		let empty
-		let count = 1
 
-		function createTable() {
 
-			const gameName = document.createElement('h2')
-			gameName.innerText = 'Miner!'
-			container.appendChild(gameName)
+let countUsers = 0
+let loginsUsedArr = []
 
-			const table = document.createElement('table')
-			for (let i = 0; i < row; i++) {
-				const tr = document.createElement('tr')
-				for (let k = 0; k < colm; k++) {
-					const td = document.createElement('td')
-					td.classList.add('squere')
-					td.innerHTML = count
-					count++
-					tableArr.push(td)
-					tr.appendChild(td)
-				}
-				table.appendChild(tr)
-			}
-			container.appendChild(table)
-			const info = document.createElement('div')
-			info.classList.add('info')
-			const p = document.createElement('p')
-
-			for (let i = 0; i < mines; i++) {
-				do {
-					mine = 0 + Math.floor(Math.random() * ((tableArr.length - 1) - 0 + 1))
-				} while (minesArr.includes(mine))
-				minesArr.push(mine)
-			}
-			minesArr.sort((a, b) => a - b)
-			empty = tableArr.length - mines
-			p.innerHTML = 'Количество пустых ячеек: ' + empty + `<br>Количество мин: ` + mines + `<br>Мин в ячейках индекс: ` + minesArr
-			info.appendChild(p)
-			container.appendChild(info)
-		}
-		createTable()
-
-		function heat(event) {
-			if (event.target.classList.contains('squere')) {
-				const p = document.querySelector('.info p')
-				if (minesArr.includes(tableArr.indexOf(event.target))) {
-					minesArr.splice(minesArr.indexOf(tableArr.indexOf(event.target)), 1)
-					event.target.style.backgroundColor = 'green';
-					event.target.classList.remove('squere')
-					mines--
-					p.innerHTML = 'Количество пустых ячеек: ' + empty + `<br>Количество мин: ` + mines + `<br>Мины в ячейках индекс :` + minesArr;
-					if (mines === 0)
-						alert('pabeditel!')
-				} else {
-					empty--
-					event.target.style.backgroundColor = 'red'
-					event.target.classList.remove('squere')
-					p.innerHTML = 'Количество пустых ячеек: ' + empty + `<br>Количество мин: ` + mines + `<br>Мины в ячейках индекс :` + minesArr
-					if (empty === 0)
-						alert('loshara!')
-				}
-			}
-
-		}
-		container.addEventListener('click', heat)
-	}
+let users = {
+  checkLogin(loginName) {
+    return loginsUsedArr.some(val => val == loginName)
+  },
+  checkValidPass(pass) {
+  return pass.split('').some(val => val == ' ')
+}
 }
 
-const block = document.querySelector('.container')
 
-const game1 = new Game(5, 5, 5, block)
+document.querySelector('.form-inputs').addEventListener('click', togglePassVision)
+//
+document.querySelector('.form-inputs').addEventListener('keyup', enableBtnLogin)
+//
+document.querySelector('.pop-up__btn').addEventListener('click', closeModal)
+document.querySelector('.pop-up__close').addEventListener('click', closeModal)
+//
+document.querySelector('.btn-reg').addEventListener('click', regUser)
+
+
+
+// toggle pass vision 
+function togglePassVision(event){
+  const btn = document.getElementById('inp-pass')
+  if (event.target.classList == 'show-pass'){
+  btn.setAttribute('type','text')
+  event.target.classList.add('hide-pass')
+  event.target.classList.remove('show-pass')
+   event.target.setAttribute('src','../images/hide-pass.png')
+  } else if (event.target.classList == 'hide-pass'){
+    btn.setAttribute('type','password')
+    event.target.classList.add('show-pass')
+   event.target.classList.remove('hide-pass')
+   event.target.setAttribute('src','../images/show-pass.png')
+  }
+}
+
+
+// enabled btn login
+function enableBtnLogin(){
+  if (document.querySelectorAll('.form-input')[0].value.length > 0 && document.querySelectorAll('.form-input')[1].value.length > 0)
+    document.querySelector('.btn-login').removeAttribute('disabled')
+  else
+   document.querySelector('.btn-login').setAttribute('disabled','disabled')
+}
+
+
+// registered users
+function regUser(){
+  let checked = {
+    'login' : false,
+    'password' : false,
+  }
+  const login = document.querySelectorAll('.form-input')[0].value
+  const password = document.querySelectorAll('.form-input')[1].value
+  let popUp = document.querySelector('.pop-up')
+  let popUpText = document.querySelector('.pop-up__text')
+  let errorLog = document.querySelector('.error-login')
+  let errorPass = document.querySelector('.error-password')
+  
+  if (login == '' || login == undefined){
+    errorLog.innerText = 'Введите логин!'
+    errorLog.style.opacity = 1
+    checked.login = false
+   } else if (users.checkLogin(login)){
+    openModal()
+    //popUp.style.zIndex = 20;
+   // popUp.style.opacity = 0.9
+    popUpText.innerText = 'Пользователь с таким ником уже существует!'
+  } else {
+    errorLog.style.opacity = 0
+    checked.login = true
+  }
+  if (password == '' || password == undefined || password.length < 6){
+    errorPass.innerHTML = `Введите пароль более 5 символов!`
+    errorPass.style.opacity = 1
+    checked.password = false
+  } else if (users.checkValidPass(password) == true){
+    errorPass.innerHTML = `Пароль не должен иметь пробелы!`
+    errorPass.style.opacity = 1
+    checked.password = false
+  }else {
+    errorPass.style.opacity = 0
+    checked.password = true
+  }
+  if (checked.login == true && checked.password == true){
+    loginsUsedArr.push(login)
+   // popUp.style.zIndex = 20;
+  //  popUp.style.opacity = 0.9
+    openModal()
+    popUpText.innerText = 'Пользователь успешно зарегистрирован!'
+    
+    countUsers++
+    let user = new User(login,password)
+    users['user'+countUsers] = {
+      'login' : user.login,
+      'password' : user.password,
+    }
+    console.log(users)
+  }
+}
+
+// close modal window
+function closeModal(){
+  document.querySelector('.pop-up__inner').classList.remove('pop-up__inner--open')
+  document.querySelector('.pop-up__inner').classList.add('pop-up__inner--close')
+    document.querySelector('.pop-up').classList.remove('pop-up--open')
+}
+function openModal(){
+   document.querySelector('.pop-up__inner').classList.add('pop-up__inner--open')
+   document.querySelector('.pop-up').classList.add('pop-up--open')
+   document.querySelector('.pop-up__inner').classList.remove('pop-up__inner--close')
+}
